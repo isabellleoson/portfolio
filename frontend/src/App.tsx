3ccc51bf84126683093b574aac09a52ffcbfd664
Isabell <isabell.leoson@gmail.com>
2023-07-31 23:38:10 +0200

    moved proxy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import Root from "./views/Root";
+import Home from "./views/Home";
+import DevProjects from "./views/DevProjects";
+import Glasskiosken from "./views/Glasskiosken";
+import UrbanGreens from "./views/UrbanGreens";
+import Resume from "./views/Resume";
+
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const router = createHashRouter([
+        {
+            children: [
+                {
+                    element: <Home />,
+                    path: "/",
+                },
+                {
+                    element: <DevProjects />,
+                    path: "/devprojects",
+                },
+
+                {
+                    element: <Resume />,
+                    path: "/resume",
+                },
+                {
+                    element: <Glasskiosken />,
+                    path: "/glasskiosken",
+                },
+
+                {
+                    element: <UrbanGreens />,
+                    path: "/urbangreens",
+                },
+            ],
+            element: <Root />,
+        },
+    ]);
+
+    return (
+        <>
+            {" "}
+            <RouterProvider router={router} />
+        </>
+    );
 }
 
-export default App
+export default App;
